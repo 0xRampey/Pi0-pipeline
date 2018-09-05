17bfd5b1ea387f65c5c48b7283130772722a17a3
Utkarsh Sarawgi <usarawgi911@gmail.com>
2018-09-05 21:28:08 +0000

    BCTManager.js added

---
+++ src/js/BCTManager.js
@@ -0,0 +1,24 @@
+const {spawn} = require('child_process');
+
+function BCTManager(dispatcher) {
+	dispatcher.subscribe('playText', this.playText)
+}
+
+BCTManager.prototype.playText = function(_, metatext) {
+//	const {spawn} = require('child_process');
+	filename = metatext
+	playfile = filename + '.wav'
+	const ls = spawn('espeak', ['-w', playfile, metatext]);
+//	const lz = spawn('aplay', [playfile]);
+
+	ls.stdout.on('data', function(data){
+		console.log(`stdout: ${data}`);
+		const lz = spawn('aplay', [playfile]);
+		lz.stdout.on('data', function(data){
+			console.log(`stdout: $(data)`);
+		});
+	});
+//	lz.stdout.on('data', function(data){
+//		console.log(`stdout: $(data)`);
+//	});	
+}
