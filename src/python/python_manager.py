f32c89b1b3cfe2dc8d0c37492fad42646718a356
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2018-09-09 16:47:52 -0400

    Continuous object detection done!

--- src/python/python_manager.py
+++ src/python/python_manager.py
@@ -1,15 +1,19 @@
 import sys
 import select
 import time
+import os
+
 # Importing tasks and all their dependencies during setup time
 import take_pic
+from objectDetector import detectObjects 
 
 def process(line):
-  if line == "takePicture":
-      print("Executing")
-      take_pic.main()
   print('read input:', line, end='')
-  sys.stdout.flush()
+  if line == "takePicture":
+    take_pic.main()
+  elif line == "detectObjects":
+    detectObjects.main()
+  
 
 # If there's input ready, do something, else do something
 # else. Note timeout is zero so select won't block at all.
@@ -17,7 +21,6 @@ print("Python manager waiting for stdin")
 while(True):
     line = sys.stdin.readline()
     if line:
-      sys.stdout.flush()
       # Remove all spaces present in the line
       line = "".join(line.split())
       process(line)
