57755c169cd821d22ff102ac3d0de0e0eac2eb2c
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2018-09-09 16:47:52 -0400

    Continuous object detection done!

--- src/index.js
+++ src/index.js
@@ -7,21 +7,32 @@ var BCTManager = require('./js/BCTManager.js')
 dispatcher.immediateExceptions = true;
 
 function DemoManager(dispatcher) {
-    this.demos = { faceDetectionStandalone: this.faceDetectionStandalone, ObjectDetectionStandalone: null};
+    this.demos = { faceDetectionStandalone: this.faceDetectionStandalone, objectDetectionStandalone: this.objectDetectionStandalone};
     faceDetectionStandalone = function() {        
 dispatcher.publish('runTask', {name: 'face_recognize', mode: 'single'});
     }
+
+    objectDetectionStandalone = function () {
+        dispatcher.publish('runTask', { name: 'objectDetection', mode: 'single' });
+    }
+
+    this.runDemo = function (_, demo) {
+        console.log("Got request to run demo", demo)
+
+        if (demo.name === 'FaceDetectionStandalone') {
+            this.faceDetectionStandalone()
+        }
+        if (demo.name === 'objectDetectionStandalone') {
+            objectDetectionStandalone()
+        }
+
+    }
+
     dispatcher.subscribe('runDemo', this.runDemo)
     dispatcher.subscribe('LongPress', this.onLongPress)
 }
 
-DemoManager.prototype.runDemo = function(_, demo) {
 
-    if(demo.name === 'FaceDetectionStandalone') {
-        faceDetectionStandalone()
-    }
-
-}
 
 DemoManager.prototype.onLongPress = function (_, meta) {
 
@@ -34,11 +45,12 @@ var PythonShell = require('python-shell');
 var options = {
   mode: 'text',
   pythonPath: 'python3',
+  pythonOptions: ['-u'],
   scriptPath: 'python/',
 };
 
 function TaskManager(dispatcher) {
-    this.tasks={ "takePicture": this.takePicture, "face_recognize": this.recognize_face, "object_detect": this.detect_objects}
+    this.tasks={ "takePicture": this.takePicture, "face_recognize": this.recognize_face, "object_detect": this.detectObjects}
 
     pyshell = new PythonShell('python_manager.py', options);
     pyshell.send("evrv")
@@ -47,6 +59,11 @@ function TaskManager(dispatcher) {
     });
     pyshell.on('close', function () {
         console.log("Manager closed!")
+    })
+
+    pyshell.on('error', function (error){
+        console.log(error)
+
     })
 
     recognizeFace = function () {
@@ -58,34 +75,37 @@ function TaskManager(dispatcher) {
         pyshell.send('takePicture')
     }
 
-    detect_objects = function () {
-        console.log("detecting objects")
+    detectObjects = function () {
+        console.log("Sending message to pyshell now")
+        pyshell.send('detectObjects')
     }
-    dispatcher.subscribe('runTask', this.runTask)
+
+    runTask = function (_, data) {
+        console.log("Going to run a task!")
+        if (data.name === 'face_recognize') {
+            this.recognizeFace();
+        }
+        if (data.name === 'objectDetection') {
+            this.detectObjects()
+        }
+        if (data.name === 'takePicture') {
+            console.log("Time to take a picture")
+            this.takePicture()
+        }
+
+    }
+    dispatcher.subscribe('runTask', runTask)
     dispatcher.subscribe('LongPress', this.takePicture)
 }
 
-TaskManager.prototype.runTask = function (_, data) {
-    console.log("Going to run a task!")
-    if(data.name === 'face_recognize') {
-        this.recognizeFace();
-    }
-    if (dasta.name === 'object_detect') {
-        this.detect_objects()
-    }
-    if (data.name === 'takePicture') {
-        console.log("Time to take a picture")
-        this.takePicture()
-    }
-    
-}
+
 
 
 const demoManager = new DemoManager(dispatcher)
 const taskManager = new TaskManager(dispatcher)
 const BtnManager = new ButtonManager(dispatcher)
 // const BctManager = new BCTManager(dispatcher)
-// dispatcher.publish('runTask', { name: 'takePicture'});
+dispatcher.publish('runDemo', {name: 'objectDetectionStandalone'});
 // dispatcher.publish('playText', 'talk it like you walk it');
 
 
