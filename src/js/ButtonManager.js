956d6959d31b31dbaa4ac39b183ef141547b2f61
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2018-09-11 17:56:40 -0400

    ButtonManager now working flawlessly!

--- src/js/ButtonManager.js
+++ src/js/ButtonManager.js
@@ -7,12 +7,11 @@ buttons.setTiming({
 });
 
 function ButtonManager (dispatcher) {
-    console.log("Button manager up")
-    // buttons.on('clicked', function (pin) {
-    //     console.log("Button click!")
-    //     dispatcher.publish('ButtonClick')
+    buttons.on('clicked', function (pin) {
+        console.log("Button click!")
+        dispatcher.publish('Click')
         
-    // });
+    });
     // buttons.on('double_clicked', function (pin) {
     //     console.log('User double clicked button on pin ', pin);
     // });
