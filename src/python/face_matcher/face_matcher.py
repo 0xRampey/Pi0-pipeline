2e6eb5e19d806cbf7496ef4342a48947de6b3261
cphung3 <cphung@mit.edu>
2018-09-20 20:39:12 -0400

    final fix face matcher

--- src/python/face_matcher/face_matcher.py
+++ src/python/face_matcher/face_matcher.py
@@ -198,8 +198,8 @@ def run_face_rec(camera, graph):
           face_enc_list.append(face_enc)
 
         prediction = predict(face_enc_list, FACE_MATCH_THRESHOLD)
-        cv2.imwrite('{}.png'.format(prediction),pic)
-        write_to_file('{}.png'.format(prediction))
+        cv2.imwrite('{}.png'.format(prediction[0]),pic)
+        write_to_file('{}.png'.format(prediction[0]))
         print("faceUpload: ",prediction)
         print('playMessage: ' + array_to_human(prediction))
 
